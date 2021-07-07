fd4bbc19a0f004c4837cf821e0a275d33896214b
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-07 16:18:37 +0900

    0210707 ２次完了形

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const ColorfulMessage = (props) => {
   console.log(props);
+  //分割代入
+  const { color, size, message } = props;
   const contentStyle = {
-    color: props.color,
-    fontSize: props.size,
-    message: props.message
+    color: color,
+    fontSize: size,
+    message: message
   };
-  return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{message}</p>;
 };
 export default ColorfulMessage;
