0769e0d355882912ed52aeb6dfb81d63f20f25e8
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-09 17:39:12 +0900

    2021/07/09

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  console.log(props);
+  //  console.log(props);
   //分割代入
   const { color, size, message } = props;
   const contentStyle = {
